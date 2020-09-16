766fc2cab259ed9506e3cd2195952ecd7cb8db53
deathsent <thanat.p38@gmail.com>
2020-09-16 22:46:15 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn @click="onClick()" icon class="mr-2">
+        <v-avatar>
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+        </v-avatar>
+      </v-btn>
+      <div id="profile" style="display:none">
+        <Login />
+      </div>
+      <div id="regis"></div>
+    </v-app-bar>
+
+    <v-main></v-main>
+  </v-app>
+</template>
+
+<script>
+import Login from "./components/Login.vue";
+
+export default {
+  name: "App",
+  components: {
+    Login,
+  },
+  methods: {
+    onClick() {
+      if (this.whenClick == 0) {
+        document.getElementById("profile").style.display = "block";
+        this.whenClick = 1;
+      } else {
+        document.getElementById("profile").style.display = "none";
+        this.whenClick = 0;
+      }
+    },
+    },
+    onClick2() {
+      if (this.dialog == false) {
+        document.getElementById("regis").style.display = "block";
+        this.dialog = true;
+      } else {
+        document.getElementById("regis").style.display = "none";
+        this.dialog = false;
+      }
+  },
+
+  data: () => ({
+    whenClick: 0,
+  }),
+};
+</script>
+<style>
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="popup">
+    <v-card id="logincard" style="display:block" class="mx-auto" max-width="344">
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-form v-model="valid">
+            <v-container>
+              <v-text-field
+                v-model="username"
+                :rules="userRules"
+                :counter="18"
+                label="Username"
+                maxlength="18"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :rules="userRules"
+                :counter="36"
+                label="Password"
+                type="password"
+                maxlength="36"
+                required
+              ></v-text-field>
+            </v-container>
+          </v-form>
+        </v-list-item-content>
+
+        <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn text>Login</v-btn>
+        <v-btn @click="dialog = true" id="btn-register" text>Register</v-btn>
+      </v-card-actions>
+    </v-card>
+
+    <div id="btn-regis">
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <!-- <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+        </template>-->
+        <v-card>
+          <v-card-title>
+            <span class="headline">User Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field label="Legal first name*" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Email*" required></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Password*" type="password" required></v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age*" required></v-select>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-autocomplete
+                    :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                    label="Interests"
+                    multiple
+                  ></v-autocomplete>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    name: "login",
+    valid: false,
+    username: "",
+    password: "",
+    dialog: false,
+    userRules: [
+      //   (vRec) => vRec || "Name is required",
+      //   (vRec) => vRec.length <= 6 || "Name must be less than 6 characters",
+    ],
+  }),
+};
+</script>
+
+<style>
+#popup {
+  /* position: fixed; */
+  position: fixed;
+  top: 0;
+  right: 10px;
+  margin-top: 65px;
+}
+</style>
